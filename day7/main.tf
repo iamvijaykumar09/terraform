9cd95da5c9e629fada2d65242be95c801e3e586b
iamvijaykumar09 <vkvhanuman9@gmail.com>
2025-05-15 11:39:32 +0000

    adding terraform aws secret handling

---
+++ day7/main.tf
@@ -0,0 +1,28 @@
+provider "aws"{
+    region = "ap-south-1"
+  //shared_credentials_files = [ "/workspaces/terraform/day7/credentials" ]
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+locals {
+  instance_name = "${terraform.workspace}-instance"
+}
+
+resource "aws_instance" "vm" {
+    ami = "ami-0e35ddab05955cf57"
+
+    instance_type = var.instance_type
+
+    tags = {
+        Name = local.instance_name
+    }
+}
+
+//using environmtent variables
+/*
+export AWS_ACCESS_KEY_ID = ""
+export AWS_SECRET_ACCESS_KEY = ""
+*/
